ee53b21b96700c18f3871bfe3f7ffeab8a8ef873
Lucas Anselmo <30813248+lucas62@users.noreply.github.com>
2022-07-25 18:44:18 -0300

    Add transactions context api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Modal from 'react-modal'
 import { Dashboard } from './components/Dashboard'
 import { Header } from './components/Header'
 import { NewTransactionModal } from './components/NewTransactionModal'
+import { api } from './services/api'
 import { GlobalStyle } from './styles/global'
+import { TransactionsProvider } from './TransactionsContext'
 
 Modal.setAppElement('#root')
 
@@ -20,7 +22,7 @@ export function App() {
     }
 
     return (
-        <>
+        <TransactionsProvider>
             <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
             <Dashboard />
 
@@ -30,6 +32,6 @@ export function App() {
             />
 
             <GlobalStyle />
-        </>
+        </TransactionsProvider>
     )
 }
